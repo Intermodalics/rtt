f1413477d5555442c1579b9089dea6d27826e7e8
Janosch Machowinski <janosch.machowinski@dfki.de>
2015-03-31 13:21:36 +0200

    Added Connection tracing ability.
    This patch modifies the ChannelElementBase in order to inject
    information that can be used to trace back the connections between
    the ports.

--- rtt/transports/corba/RemoteChannelElement.hpp
+++ rtt/transports/corba/RemoteChannelElement.hpp
@@ -43,6 +43,7 @@
 #include "CorbaTypeTransporter.hpp"
 #include "CorbaDispatcher.hpp"
 #include "CorbaConnPolicy.hpp"
+#include "ApplicationServer.hpp"
 
 namespace RTT {
 
@@ -70,6 +71,8 @@ namespace RTT {
 
         PortableServer::ObjectId_var oid;
 
+        std::string localUri;
+
         ConnPolicy policy;
 
         public:
@@ -93,6 +96,8 @@ namespace RTT {
                 oid = mpoa->activate_object(this);
                 // Force creation of dispatcher.
                 CorbaDispatcher::Instance(msender);
+                
+                localUri = ApplicationServer::orb->object_to_string(_this());
             }
 
             ~RemoteChannelElement()
@@ -475,6 +480,28 @@ namespace RTT {
             {
                 ConnPolicy policy = toRTT(cp);
                 return base::ChannelElement<T>::channelReady(this, policy);
+            }
+
+            virtual bool isRemoteElement() const
+            {
+                return true;
+            }
+            
+            
+            virtual std::string getRemoteURI() const
+            {
+                std::string uri = ApplicationServer::orb->object_to_string(remote_side);
+                return uri;
+            }
+            
+            virtual std::string getLocalURI() const
+            {
+                return localUri;
+            }
+            
+            virtual std::string getElementName() const
+            {
+                return "CorbaRemoteChannelElement";
             }
         };
     }
