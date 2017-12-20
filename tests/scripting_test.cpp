d2784de0c5bd084378330eb7c067b30a4243a6a7
Johannes Meyer <johannes@intermodalics.eu>
2017-12-20 19:39:21 +0100

    scripting: use preprocessor stringification to split Orocos scripting code over multiple lines
    ...for improved readability.
    
    The check of the parser result now uses BOOST_REQUIRE() to abort the test case if parsing failed.
    Follow-up checks would fail anyway if the parser failed.
    
    Signed-off-by: Johannes Meyer <johannes@intermodalics.eu>

--- tests/scripting_test.cpp
+++ tests/scripting_test.cpp
@@ -34,6 +34,8 @@ using namespace RTT::detail;
 
 #include <boost/shared_ptr.hpp>
 
+// note: Does not preserve newlines. Add them explicitly with \n or add semicolons after each line.
+#define MULTILINE_STRING(...) #__VA_ARGS__
 
 // Registers the fixture into the 'registry'
 BOOST_FIXTURE_TEST_SUITE(  ScriptingTestSuite,  OperationsFixture )
@@ -53,9 +55,11 @@ BOOST_AUTO_TEST_CASE(TestGetProvider)
     boost::shared_ptr<Scripting> sc = tc->getProvider<Scripting>("scripting");
     BOOST_REQUIRE( sc );
     BOOST_CHECK ( sc->ready() );
-    bool r;
-    r = sc->loadProgramText("program Foo { do test.assert(true); set ret = 10.0; }");
-    BOOST_REQUIRE( r );
+    BOOST_REQUIRE( sc->loadProgramText( MULTILINE_STRING(
+        program Foo {
+            do test.assert(true);
+            set ret = 10.0;
+        })));
     BOOST_CHECK( sc->hasProgram("Foo") );
     BOOST_REQUIRE( sc->startProgram("Foo") );
     BOOST_CHECK( sc->isProgramRunning("Foo") );
@@ -82,75 +86,130 @@ BOOST_AUTO_TEST_CASE(TestScriptingParser)
     boost::shared_ptr<Scripting> sc = tc->getProvider<Scripting>("scripting");
     BOOST_REQUIRE( sc );
     BOOST_CHECK ( sc->ready() );
-    bool r;
 
     // test plain statements:
-    string statements=";;test.increase()\n;;;\ntest.increase()"; // trailing newline is optional
-    r = sc->eval(statements);
-    BOOST_CHECK( r );
+    BOOST_REQUIRE( sc->eval( MULTILINE_STRING(
+        ;;test.increase()\n
+        ;;;\n
+        test.increase())));  // trailing newline is optional
     BOOST_CHECK_EQUAL( i, 1);
 
     // test variable decls:
-    statements="var int i = 0; var int y,z=10; test.i = z";
-    r = sc->eval(statements);
-    BOOST_CHECK( r );
+    BOOST_REQUIRE( sc->eval( MULTILINE_STRING(
+        var int i = 0;
+        var int y, z = 10;
+        test.i = z;
+        )));
     BOOST_CHECK_EQUAL( i, 10);
 
     // test if statement:
-    statements="var int x=1,y=2; if  3 == 8 then test.i = x else test.i = y";
-    r = sc->eval(statements);
-    BOOST_CHECK( r );
+    BOOST_REQUIRE( sc->eval( MULTILINE_STRING(
+        var int x = 1, y = 2;
+        if 3 == 8 then
+            test.i = x;
+        else
+            test.i = y;
+        )));
     BOOST_CHECK_EQUAL( i, 2);
 
     // test while statement:
-    statements="var int x=1,y=2; while x != y  { test.i = 3; x = y; }";
-    r = sc->eval(statements);
-    BOOST_CHECK( r );
+    BOOST_REQUIRE( sc->eval( MULTILINE_STRING(
+        var int x = 1, y = 2;
+        while x != y {
+            test.i = 3;
+            x = y;
+        })));
     BOOST_CHECK_EQUAL( i, 3);
 
     // test while name clash:
-    statements="var int whilex, whiley\n whilex = 1; whiley=2\n while whilex != whiley  { test.i = 3; whilex = whiley; }";
-    r = sc->eval(statements);
-    BOOST_CHECK( r );
+    BOOST_REQUIRE( sc->eval( MULTILINE_STRING(
+        var int whilex, whiley\n
+        whilex = 1;
+        whiley = 2\n
+        while whilex != whiley {
+            test.i = 3;
+            whilex = whiley;
+        })));
     BOOST_CHECK_EQUAL( i, 3);
 
     // test for statement:
-    statements="var int x=10,y=20; for(  x = 0; x != y; x = x + 1) { test.i = x; }";
-    r = sc->eval(statements);
-    BOOST_CHECK( r );
+    BOOST_REQUIRE( sc->eval( MULTILINE_STRING(
+        var int x = 10, y = 20;
+        for( x = 0; x != y; x = x + 1) {
+            test.i = x;
+        })));
     BOOST_CHECK_EQUAL( i, 19);
 
     // test for name clash:
-    statements="var int forx,fory\n forx=10; fory=20; for(  forx = 0; forx != fory; forx = forx + 1) { test.i = forx; }";
-    r = sc->eval(statements);
-    BOOST_CHECK( r );
+    BOOST_REQUIRE( sc->eval( MULTILINE_STRING(
+        var int forx, fory\n
+        forx = 10; fory = 20;
+        for( forx = 0; forx != fory; forx = forx + 1) {
+            test.i = forx;
+        })));
     BOOST_CHECK_EQUAL( i, 19);
 
     // test function +  a statement that uses that function:
-    statements = "export function adder(int a, int b) { test.i = a + b; }\n adder(5,6)\n";
-    r = sc->eval(statements);
-    BOOST_CHECK( r );
+    BOOST_REQUIRE( sc->eval( MULTILINE_STRING(
+        export function adder(int a, int b) {
+            test.i = a + b;
+        }\n
+        adder(5,6)\n
+        )));
     BOOST_CHECK_EQUAL( i, 11);
-    statements = "export void adder2(int a, int b) { test.i = a + b; }\n adder2(7,8)\n";
-    r = sc->eval(statements);
-    BOOST_CHECK( r );
+    BOOST_REQUIRE( sc->eval( MULTILINE_STRING(
+        export void adder2(int a, int b) {
+            test.i = a + b;
+        }\n
+        adder2(7,8)\n
+        )));
     BOOST_CHECK_EQUAL( i, 15);
-    statements = "export int adder3(int a, int b) { return a + b; }\n test.i = adder3(6,10)\n";
-    r = sc->eval(statements);
-    BOOST_CHECK( r );
+    BOOST_REQUIRE( sc->eval( MULTILINE_STRING(
+        export int adder3(int a, int b) {
+            return a + b;
+        }\n
+        test.i = adder3(6,10)\n
+        )));
     BOOST_CHECK_EQUAL( i, 16);
 
     // test program +  a statement that starts that program and waits for the result.
-    statements = "program rt_script { test.i = 3-9; }\n rt_script.start();;;; while( rt_script.isRunning() ) { trigger(); yield; }\n";
-    r = sc->eval(statements);
-    BOOST_CHECK( r );
+    BOOST_REQUIRE( sc->eval( MULTILINE_STRING(
+        program rt_script {
+            test.i = 3-9;
+        }\n
+        rt_script.start();;;;
+        while( rt_script.isRunning() ) {
+            trigger();
+            yield;
+        })));
     BOOST_CHECK_EQUAL( sc->getProgramStatus("rt_script"), ProgramInterface::Status::stopped );
     BOOST_CHECK_EQUAL( i, -6);
 
     // test state machine +  a statement that starts that SM and waits for the result.
-    statements = "StateMachine RTState { initial state init { entry { test.i = 0;} transitions { select fini; } } final state fini { entry { test.i = test.i - 2; } } }; RootMachine RTState rt_state; rt_state.activate(); rt_state.start(); while( !rt_state.inFinalState() ) { trigger(); yield; }\n";
-    r = sc->eval(statements);
-    BOOST_CHECK( r );
+    BOOST_REQUIRE( sc->eval( MULTILINE_STRING(
+        StateMachine RTState {
+            initial state init {
+                entry {
+                    test.i = 0;
+                }
+                transitions {
+                    select fini;
+                }
+            }
+
+            final state fini {
+                entry {
+                   test.i = test.i - 2;
+                }
+            }
+        }
+        RootMachine RTState rt_state;
+        rt_state.activate();
+        rt_state.start();
+        while( !rt_state.inFinalState() ) {
+            trigger();
+            yield; // ...has no effect here other than incrementing the step counter! See ScriptParser::seenstatement().
+        })));
     BOOST_CHECK_EQUAL( sc->getStateMachineState("rt_state"), "fini" );
     BOOST_CHECK_EQUAL( i, -2);
 }
@@ -167,112 +226,101 @@ BOOST_AUTO_TEST_CASE(TestScriptingFunction)
     boost::shared_ptr<Scripting> sc = tc->getProvider<Scripting>("scripting");
     BOOST_REQUIRE( sc );
     BOOST_CHECK ( sc->ready() );
-    bool r;
 
     // set test counter to zero:
     i = 0;
 
     // define a function (added to scripting interface):
-    string statements="void func1(void) { test.increase(); }\n";
-    r = sc->eval(statements);
-    BOOST_CHECK( r );
+    BOOST_REQUIRE( sc->eval( MULTILINE_STRING(
+        void func1(void) {
+            test.increase();
+        })));
     BOOST_CHECK_EQUAL( i, 0);
     BOOST_CHECK( tc->provides("scripting")->hasMember("func1"));
 
     // export a function:
-    statements="export void efunc1(void) { test.increase(); }\n";
-    r = sc->eval(statements);
-    BOOST_CHECK( r );
+    BOOST_REQUIRE( sc->eval( MULTILINE_STRING(
+        export void efunc1(void) {
+            test.increase();
+        })));
     BOOST_CHECK_EQUAL( i, 0);
     BOOST_CHECK( tc->provides()->hasMember("efunc1"));
 
     // local function:
-    statements="void lfunc1(void) { test.increase(); }\n";
-    r = sc->eval(statements);
-    BOOST_CHECK( r );
+    BOOST_REQUIRE( sc->eval( MULTILINE_STRING(
+        void lfunc1(void) {
+            test.increase();
+        })));
     BOOST_CHECK_EQUAL( i, 0);
     BOOST_CHECK( tc->provides("scripting")->hasMember("lfunc1"));
 
     // global function:
-    statements="global void gfunc1(void) { test.increase(); }\n";
-    r = sc->eval(statements);
-    BOOST_CHECK( r );
+    BOOST_REQUIRE( sc->eval( MULTILINE_STRING(
+        global void gfunc1(void) {
+            test.increase();
+        })));
     BOOST_CHECK_EQUAL( i, 0);
     BOOST_CHECK( GlobalService::Instance()->provides()->hasMember("gfunc1"));
 
     // invoke a function:
-    statements="func1()\n";
-    r = sc->eval(statements);
-    BOOST_CHECK( r );
+    BOOST_REQUIRE( sc->eval("func1()") );
     BOOST_CHECK_EQUAL( i, 1);
 
     // invoke an exported function:
-    statements="efunc1()\n";
-    r = sc->eval(statements);
-    BOOST_CHECK( r );
+    BOOST_REQUIRE( sc->eval("efunc1()") );
     BOOST_CHECK_EQUAL( i, 2);
 
     // invoke a global function:
-    statements="gfunc1()\n";
-    r = sc->eval(statements);
-    BOOST_CHECK( r );
+    BOOST_REQUIRE( sc->eval("gfunc1()") );
     BOOST_CHECK_EQUAL( i, 3);
 
     // invoke a local function:
-    statements="lfunc1()\n";
-    r = sc->eval(statements);
-    BOOST_CHECK( r );
+    BOOST_REQUIRE( sc->eval("lfunc1()") );
     BOOST_CHECK_EQUAL( i, 4);
 
     // call a function:
-    statements="call func1()\n";
-    r = sc->eval(statements);
-    BOOST_CHECK( !r );
+    BOOST_CHECK( !sc->eval("call func1()") );
     BOOST_CHECK_EQUAL( i, 4);
 
     // call an exported function:
-    statements="call efunc1()\n";
-    r = sc->eval(statements);
-    BOOST_CHECK( !r );
+    BOOST_CHECK( !sc->eval("call efunc1()") );
     BOOST_CHECK_EQUAL( i, 4);
 
     // RE-define a function (added to scripting interface):
-    statements="void func1(void) { test.increase(); test.increase(); }\n";
-    r = sc->eval(statements);
-    BOOST_CHECK( r );
+    BOOST_REQUIRE( sc->eval( MULTILINE_STRING(
+        void func1(void) {
+            test.increase();
+            test.increase();
+        })));
     BOOST_CHECK_EQUAL( i, 4);
     BOOST_CHECK( tc->provides("scripting")->hasMember("func1"));
 
-    statements="func1()\n";
-    r = sc->eval(statements);
-    BOOST_CHECK( r );
+    BOOST_REQUIRE( sc->eval("func1()") );
     BOOST_CHECK_EQUAL( i, 6);
 
     // RE-export a function:
-    statements="export void efunc1(void) { test.increase(); test.increase();}\n";
-    r = sc->eval(statements);
-    BOOST_CHECK( r );
+    BOOST_REQUIRE( sc->eval( MULTILINE_STRING(
+        export void efunc1(void) {
+            test.increase();
+            test.increase();
+        })));
     BOOST_CHECK_EQUAL( i, 6);
     BOOST_CHECK( tc->provides()->hasMember("efunc1"));
 
-    statements="efunc1()\n";
-    r = sc->eval(statements);
-    BOOST_CHECK( r );
+    BOOST_REQUIRE( sc->eval("efunc1()") );
     BOOST_CHECK_EQUAL( i, 8);
 
     // RE-global a function:
-    statements="global void gfunc1(void) { test.increase(); test.increase();}\n";
-    r = sc->eval(statements);
-    BOOST_CHECK( r );
+    BOOST_REQUIRE( sc->eval( MULTILINE_STRING(
+        global void gfunc1(void) {
+            test.increase();
+            test.increase();
+        })));
     BOOST_CHECK_EQUAL( i, 8);
     BOOST_CHECK( GlobalService::Instance()->provides()->hasMember("gfunc1"));
 
-    statements="gfunc1()\n";
-    r = sc->eval(statements);
-    BOOST_CHECK( r );
+    BOOST_REQUIRE( sc->eval("gfunc1()") );
     BOOST_CHECK_EQUAL( i, 10);
-
-
 }
 
 BOOST_AUTO_TEST_SUITE_END()
